e9f82f575486a80208bede5657f4949083e2e0b8
shashank chaudhary <shashank.c2048@gmail.com>
2021-02-08 01:00:04 +0530

    app change 3

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pickle
 app = Flask(__name__)
 model = pickle.load(open('randomForestRegressor.pkl','rb'))
 
-#hello world
+#hello world india
 @app.route('/')
 def home():
     #return 'Hello World'
